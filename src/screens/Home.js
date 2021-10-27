020b261e2fd7976b6cd54ae1d3b47d8eb8d18cd1
Tormod Flesjø <tormod.flesjo@gmail.com>
2021-10-27 16:23:10 +0200

    add landing page

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -8,13 +8,18 @@ const Home = () => {
     <main className='text-white h-100 relative '>
       <img src={landingImage} alt='front door' className="object-cover w-full h-full" />
       <div className="absolute top-8 left-16">
-        <h1 className="text-4xl font-bold">Welcome to the Home of the Future</h1>
-        <p className="text-xl"> We are a group of students who are passionate about the future of the world. We are here to help you find the best way to get there. </p></div>
+        <h1 className="text-4xl font-bold">Leiligheten</h1>
+        <p className="text-xl"> Lorem impsum </p></div>
       <div className="absolute bottom-8 left-16">
-        <p className="text-4xl font-bold">We are here to help you find the best way to get there.</p>
+        <p className="text-xl font-bold">Lorem lorem.</p>
       </div>
-      <div className="absolute top-8 right-16">Top Right</div>
-      <div className="absolute bottom-8 right-16">Bottom Right</div>
+      <div className="absolute top-8 right-16">
+      <h1 className="text-4xl font-bold">Galleriet</h1>
+      <p className="text-xl font-bold">Lorem lorem.</p>
+      </div>
+      <div className="absolute bottom-8 right-16">
+        <p className="text-xl font-bold">Lorem lorem.</p>
+      </div>
     </main>
 
   );
