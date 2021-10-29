d10f00f37a8c291086a9b56f53670407b1ccd8ad
Tormod Flesjø <tormod.flesjo@gmail.com>
2021-10-29 09:40:13 +0200

    fix float issue

--- src/components/BigHeader.jsx
+++ src/components/BigHeader.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 const BigHeader = ({title, subTitle, alignedLeft}) => {
     const textAlignment = alignedLeft ? "text-left" : "text-right"
+    const floatHr = alignedLeft ? "float-left" : "float-right"
     return (
             <div className=''>
-              <h1 className={`font-bold text-xl lg:text-6xl uppercase ${textAlignment}`}>{title}</h1>
-              <hr className="w-full border-2 mb-6"/>
-            <h2 className={`text-sm lg:text-6xl swhong-font ${textAlignment}`}> {subTitle} </h2>
+              <h1 className={`font-bold text-3xl lg:text-6xl uppercase ${textAlignment}`}>{title}</h1>
+              <hr className={`w-11/12 lg:w-full border-2 mb-6 ${floatHr}`}/>
+            <h2 className={`text-4xl lg:text-6xl swhong-font ${textAlignment}`}> {subTitle} </h2>
         </div>
     )
 }
